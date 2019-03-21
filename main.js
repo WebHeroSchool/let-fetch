2614cd37a41b97d32ec6c615a0dde90640b5af5b
Evgeniia Grik <evgeniyagrik@gmail.com>
2019-03-21 22:53:46 +0300

    now url for request is provided from profile url

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ let pic = document.querySelector('img.user-info__pic');
 pic.setAttribute('alt', 'User Avatar');
 let bio = document.querySelector('p.user-info__description');
 let link;
-fetch('https://api.github.com/users/6thSence')
+let userUrl = 'https://github.com/azizmuradovar';
+let apiUrl = 'https://api.github.com/users/' + userUrl.slice(19);
+fetch(apiUrl)
     .then(response => response.json())
     .then(json => { 
         user = Object.assign({}, json);
@@ -14,6 +16,8 @@ fetch('https://api.github.com/users/6thSence')
         title.innerHTML = user.name;
         name.innerHTML = user.name;
         pic.setAttribute('src', user.avatar_url);
+        user.bio == null ? 
+        bio.innerHTML = 'No bio provided.' + '\n' :
         bio.innerHTML = user.bio + '\n';
         link = document.createElement('a');
         link.innerHTML = 'Show profile';
