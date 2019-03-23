6ea4be1d21ffa6769b8853d3f7b8af12d456ddc8
Evgeniia Grik <evgeniyagrik@gmail.com>
2019-03-23 20:50:02 +0300

    show profile link, no img initially

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 let title = document.head.children[1];
 let name = document.querySelector('h1.user__name');
-let pic = document.querySelector('img.user-info__pic');
-pic.setAttribute('alt', 'User Avatar');
 let bio = document.querySelector('p.user-info__description');
-let link;
 
 let searchParams = new URLSearchParams(window.location.search);
 let login = searchParams.get('username');
@@ -15,13 +12,20 @@ fetch(apiUrl)
         if (user.name) {
             title.innerHTML = user.name;
             name.innerHTML = user.name;
-            pic.setAttribute('src', user.avatar_url);
+            
             user.bio === null ? 
             bio.innerHTML = 'No bio provided.' + '\n' :
             bio.innerHTML = user.bio + '\n';
-            link = document.createElement('a');
+            
+            let pic = document.createElement('img');
+            pic.className = 'img.user-info__pic';
+            pic.setAttribute('alt', 'User Avatar');
+            pic.setAttribute('src', user.avatar_url);
+            document.body.insertBefore(pic, bio);
+            
+            let link = document.createElement('a');
             link.innerHTML = 'Show profile';
-            link.setAttribute('href', user.url);
+            link.setAttribute('href', user.html_url);
             bio.appendChild(link);
         }
     })
