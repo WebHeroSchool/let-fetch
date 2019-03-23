4859a2f372f573b324a49440dbbdc69916444af6
Evgeniia Grik <evgeniyagrik@gmail.com>
2019-03-23 21:17:04 +0300

    img problem in process 2

--- main.js
+++ main.js
@@ -18,10 +18,11 @@ fetch(apiUrl)
             bio.innerHTML = user.bio + '\n';
             
             let pic = document.createElement('img');
-            pic.className = 'img.user-info__pic';
+            pic.className = 'user-info__pic';
             pic.setAttribute('alt', 'User Avatar');
             pic.setAttribute('src', user.avatar_url);
             console.log(pic);
+            console.log(bio);
             document.body.insertBefore(pic, bio);
             
             let link = document.createElement('a');
