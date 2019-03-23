9dcf26d2e7646d87240b85d8b8c19e0967b82a1a
Evgeniia Grik <evgeniyagrik@gmail.com>
2019-03-23 21:19:06 +0300

    img problem: declared vars before fetching

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let title = document.head.children[1];
 let name = document.querySelector('h1.user__name');
 let bio = document.querySelector('p.user-info__description');
+let pic;
+let link;
 
 let searchParams = new URLSearchParams(window.location.search);
 let login = searchParams.get('username');
@@ -17,7 +19,7 @@ fetch(apiUrl)
             bio.innerHTML = 'No bio provided.' + '\n' :
             bio.innerHTML = user.bio + '\n';
             
-            let pic = document.createElement('img');
+            pic = document.createElement('img');
             pic.className = 'user-info__pic';
             pic.setAttribute('alt', 'User Avatar');
             pic.setAttribute('src', user.avatar_url);
@@ -25,7 +27,7 @@ fetch(apiUrl)
             console.log(bio);
             document.body.insertBefore(pic, bio);
             
-            let link = document.createElement('a');
+            link = document.createElement('a');
             link.innerHTML = 'Show profile';
             link.setAttribute('href', user.html_url);
             bio.appendChild(link);
